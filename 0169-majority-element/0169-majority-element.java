db38d4dbea309df3dde8a2322f840b2ef8a3dfa0
Hiran <hirankrishna922@gmail.com>
2026-08-13 15:40:33 +0530

    Time: 1 ms (99.89%), Space: 55.6 MB (46.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,30 +1,18 @@
-import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for(int num : nums)
+        {
+            if(count == 0) candidate = num;
+            if (num == candidate) count++;
+            else count--;
+        }
+
+        return candidate;
+
         
-        int tresh = (nums.length)/2;
-        Hashtable<Integer, Integer> map = new Hashtable<>();
-        for(int i = 0; i<nums.length; i++)
-        {
-            if(!map.containsKey(nums[i])) map.put(nums[i],1);
-
-            else
-            {
-                int current = map.get(nums[i]);
-                map.put(nums[i],current+1);
-            }
-        }
-
-        for(var entry : map.entrySet())
-        {
-            int n = entry.getKey();
-            int count = entry.getValue();
-
-            if (count > tresh)
-            {
-                return n;
-            } 
-        }
-        return 0;
+        
     }
 }
